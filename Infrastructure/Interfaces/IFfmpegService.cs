bda587078b250905a0dcf2e233c3b965cb8fc53e
Kristifor Milchev <km@airzen.io>
2023-10-26 22:07:46 +0300

    Implementing the chunk transcoder.

--- Infrastructure/Interfaces/IFfmpegService.cs
+++ Infrastructure/Interfaces/IFfmpegService.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Interfaces;
 
 public interface IFfmpegService
 {
-    public string GetChunk(int start, int end, string file);
+    public string GetChunk(int start, int end, string file, string name);
     public Task<string> ConvertTo(IFormFile data,VideoType type, string outputFile = "output_video.mp4");
     public Task<string> ConvertToBinary(string path,VideoType type, string outputFile = "output_video.mp4");
     public TimeSpan GetMovieLenght(string file);
