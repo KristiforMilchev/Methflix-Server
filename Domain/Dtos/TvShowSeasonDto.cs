96be22254b18b110963aa02154df6fde33524514
Kristifor Milchev <km@airzen.io>
2023-11-04 18:39:39 +0200

    Adding constructor to the tv show season dto.

--- Domain/Dtos/TvShowSeasonDto.cs
+++ Domain/Dtos/TvShowSeasonDto.cs
@@ -2,6 +2,10 @@ namespace Domain.Dtos;
 
 public class TvShowSeasonDto
 {
+    public TvShowSeasonDto()
+    {
+        Seasons = new List<SeasonData>();
+    }
     public string Name { get; set; }
     public List<SeasonData> Seasons { get; set; }
 }
